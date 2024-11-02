13ce380b7db982c288684b3fdd8bf9c42794667d
TrangMai288 <trangmai828@gmail.com>
2024-11-02 16:40:23 +0700

    update and run all package

---
+++ src/test/java/theInternet/HoverTest1.java
@@ -0,0 +1,28 @@
+package theInternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HoverTest1 {
+    @Test
+    void ableToHoverImage1(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/hovers");
+
+        Actions actions = new Actions(driver);
+
+        actions
+                .moveToElement(
+                        driver
+                                .findElements(By.cssSelector("#content .figure"))
+                                .get(1))
+                .perform();
+//        String imageName = driver.findElement(By.xpath("//*[@id='content']//div[@class='figure'][2]//h5")).getText();
+        String imageName = driver.findElements(By.cssSelector(".figcaption h5")).get(0).getText();
+        Assert.assertEquals(imageName,"name: user2");
+    }
+}
---
+++ src/test/java/theInternet/JSAlertTest.java
@@ -0,0 +1,56 @@
+package theInternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class JSAlertTest {
+    WebDriver driver;
+
+    @BeforeClass
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+    }
+
+    @Test
+    void closeJsAlertSuccessfully(){
+        driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
+
+        driver.switchTo().alert().accept();
+        Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You successfully clicked an alert");
+    }
+
+    @Test
+    void verifyClickOKinJsAlertConfirmSuccessfully(){
+        driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
+
+        driver.switchTo().alert().accept();
+        Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You clicked: Ok");
+    }
+
+    @Test
+    void verifyClickCancelinJsAlertConfirmSuccessfully(){
+        driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
+
+        driver.switchTo().alert().dismiss();
+        Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You clicked: Cancel");
+    }
+
+    @Test
+    void verifyInputJsAlertPromtSuccessfully(){
+        driver.findElement(By.xpath("//button[.='Click for JS Prompt']")).click();
+
+        driver.switchTo().alert().sendKeys("hello");
+        driver.switchTo().alert().accept();
+
+        Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You entered: hello");
+    }
+
+    @AfterClass
+    void tearDown() {
+        driver.quit();
+    }
+}
