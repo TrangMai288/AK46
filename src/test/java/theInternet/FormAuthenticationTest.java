c26eca4366525ed67c5d76609d412aacab08ec8f
TrangMai288 <trangmai828@gmail.com>
2024-11-14 00:08:20 +0700

    full code day 8 about page object model

--- src/test/java/theInternet/FormAuthenticationTest.java
+++ src/test/java/theInternet/FormAuthenticationTest.java
@@ -1,94 +1,30 @@
 package theInternet;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import supports.Browser;
+import theInternet.pages.FormAuthenticationPage;
 
 public class FormAuthenticationTest {
-    WebDriver driver;
-    /**
-     *
-     1. Open browser
-     2. Navigate to https://the-internet.herokuapp.com/login
-     3. Fill in username with tomsmith
-     4. Fill in the password with SuperSecretPassword!
-     5. Click on Login button
-     6. And the home page is appear
-     */
-    @BeforeMethod
+    @BeforeClass
     void setup() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless=new");
-
-        driver = new ChromeDriver(chromeOptions);
+        Browser.openBrowser("chrome");
     }
 
     @Test
     void shouldBeSuccessfullyWithValidCredentials() {
-        driver.get("https://the-internet.herokuapp.com/login");
+        FormAuthenticationPage formAuthenticationPage = new FormAuthenticationPage();
+        formAuthenticationPage.open();
+        formAuthenticationPage.login("tomsmith", "SuperSecretPassword!");
 
-        // tagName=input
-//      driver.findElement(By.tagName("input")).sendKeys("tomsmith");
-
-        // type=text
-//      driver.findElement(By.cssSelector("[type=text]")).sendKeys("tomsmith");
-//      driver.findElement(By.xpath("//*[@type='text']")).sendKeys("tomsmith");
-
-        // name=username
-//        driver.findElement((By.name("username"))).sendKeys("tomsmith");
-//        driver.findElement(By.cssSelector("[name=username")).sendKeys("tomsmith");
-//        driver.findElement(By.xpath("//*[@name='username']")).sendKeys("tomsmith");
-
-        // id=username
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-//        driver.findElement(By.cssSelector("#username")).sendKeys("tomsmith");
-//        driver.findElement(By.xpath("//*[@id='username']")).sendKeys("tomsmith");
-
-        // tagName=input
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-
-//        driver.findElement(By.className("radius")).click();
-//        driver.findElement(By.cssSelector(".radius")).click();
-//        driver.findElement(By.xpath("//*[@class='radius']")).click();
-//
-//        driver.findElement(By.cssSelector("type=submit")).click();
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
-
-
-        // verify current url = https://the-internet.herokuapp.com/secure
-        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/secure");
-
-        // verify message You logged into a secure area!
-        Assert.assertTrue(driver.findElement(By.className("success")).getText().contains("You logged into a secure area!"));
-
-        driver.quit();
+        Assert.assertEquals(Browser.getCurrentUrl(), "https://the-internet.herokuapp.com/secure");
+        Assert.assertTrue(formAuthenticationPage.isLoggedIn());
     }
 
-    @Test
-    void loginUnsuccessfulWhenEnteringWrongUsername() {
-        driver.get("https://the-internet.herokuapp.com/login");
-
-        driver.findElement(By.id("username")).sendKeys("a");
-        driver.findElement(By.id("password")).sendKeys("tomsmith1");
-
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/login");
-        Assert.assertTrue(driver.findElement(By.className("error")).getText().contains( "Your username is invalid!"));
-    }
-
-    @Test
-    void loginUnsuccessfulWhenEnteringWrongPassword() {
-        driver.get("https://the-internet.herokuapp.com/login");
-
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("a");
-
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/login");
-        Assert.assertTrue(driver.findElement(By.className("error")).getText().contains( "Your password is invalid!"));
+    @AfterClass
+    void tearDown() {
+        Browser.quit();
     }
 }
--- src/test/java/theInternet/HorizontalSliderTest.java
+++ src/test/java/theInternet/HorizontalSliderTest.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -18,30 +20,31 @@ import java.time.Duration;
 
 public class HorizontalSliderTest {
     WebDriver driver;
+    Actions actions;
 
     @BeforeClass
     void setUp() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless=new");
         driver = new ChromeDriver(chromeOptions);
+        actions = new Actions(driver);
     }
 
-//    @Test
-//    void ableToHorizontalSlidePointer() throws InterruptedException {
-//        driver.get("https://the-internet.herokuapp.com/horizontal_slider");
-//
-//        Actions actions = new Actions(driver);
-//        WebElement pointer = driver.findElement(By.cssSelector(".sliderContainer input"));
-//        int offsetWidth = pointer.getSize().getWidth();
-//        int offsetHeight = pointer.getSize().getHeight();
-//        System.out.printf("%d %d", offsetHeight,offsetWidth);
-//        actions.clickAndHold(pointer)
-//                .moveByOffset(offsetWidth,0)
-//                .perform();
-//
-//        Thread.sleep(60000); //wait until next change
-//        Assert.assertEquals(driver.findElement(By.id("range")).getText(),"5");
-//    }
+    @Test
+    void ableToHorizontalSlidePointer() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/horizontal_slider");
+
+        WebElement pointer = driver.findElement(By.cssSelector(".sliderContainer input"));
+        int offsetWidth = pointer.getSize().getWidth();
+        int offsetHeight = pointer.getSize().getHeight();
+        System.out.printf("%d %d", offsetHeight,offsetWidth);
+        actions.clickAndHold(pointer)
+                .moveByOffset(offsetWidth,0)
+                .perform();
+
+        Thread.sleep(1000); //wait until next change
+        Assert.assertEquals(driver.findElement(By.id("range")).getText(),"5");
+    }
 
 //    @Test
 //    void ableToHorizontalSlidePointerUsingWait() throws InterruptedException {
@@ -66,8 +69,6 @@ public class HorizontalSliderTest {
     void ableScrollDown() throws InterruptedException {
         driver.get("https://the-internet.herokuapp.com/infinite_scroll");
 
-        Actions actions = new Actions(driver);
-
         for (int i = 0; i < 5; i++) {
             actions.scrollByAmount(0,1000).perform();
             Thread.sleep(1000);
@@ -78,7 +79,6 @@ public class HorizontalSliderTest {
     void contextClick(){
         driver.get("https://the-internet.herokuapp.com/context_menu");
 
-        Actions actions = new Actions(driver);
         actions.contextClick(driver.findElement(By.id("hot-spot"))).perform();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.alertIsPresent()).accept();
@@ -87,7 +87,6 @@ public class HorizontalSliderTest {
     @Test
     void keyPress(){
         driver.get("https://the-internet.herokuapp.com/key_presses");
-        Actions actions = new Actions(driver);
 
         actions.keyDown(Keys.ESCAPE).perform();
 
@@ -102,6 +101,16 @@ public class HorizontalSliderTest {
         File srcFile = scrShot.getScreenshotAs(OutputType.FILE);
         File destFile = new File("target/selenium.png");
         FileUtils.copyFile(srcFile, destFile);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    void captureScreenshot(ITestResult testResult) throws IOException {
+        if(!testResult.isSuccess()){
+            TakesScreenshot scrShot =((TakesScreenshot)driver);
+            File srcFile = scrShot.getScreenshotAs(OutputType.FILE);
+            File destFile = new File(String.format("target/%s-%d.png",testResult.getMethod().getMethodName(),System.currentTimeMillis()));
+            FileUtils.copyFile(srcFile, destFile);
+        }
     }
 
     @AfterClass
